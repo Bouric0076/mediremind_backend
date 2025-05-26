0aa678a92a9e81b58d954109d1566c2a7abb897f
Bouric Enos <bouricenos0@gmail.com>
2025-05-26 11:25:54 +0300

    mediremind_backend changed debug to false

--- mediremind_backend/mediremind_backend/settings.py
+++ mediremind_backend/mediremind_backend/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-your-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['199c-41-60-238-242.ngrok-free.app', 'localhost', '127.0.0.1']
 
